5f8bc63795d1bbcce08ce6de0ff0e36c57571748
vlikshiney7 <vlikshiney7@laposte.net>
2019-11-07 15:30:33 +0100

    Un commit random

--- ProjetGestionGarages/src/main/java/com/garage/service/ServicePiece.java
+++ ProjetGestionGarages/src/main/java/com/garage/service/ServicePiece.java
@@ -4,52 +4,47 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.garage.bean.Piece;
 import com.garage.dao.DaoPiece;
 import com.garage.iservice.IServicePiece;
 
-/**
- * 
- */
 @Service
 public class ServicePiece implements IServicePiece {
-DaoPiece daoPiece;
-	/**
-	 * Default constructor
-	 */
-	public ServicePiece() {
-	}
+	
+	@Autowired
+	DaoPiece daoPiece;
+	
 	@Transactional
 	@Override
 	public void ajouterPiece(Piece piece) {
 		daoPiece.save(piece);
-		
 	}
+	
 	@Transactional
 	@Override
 	public List<Piece> recherchePiece() {
-		// TODO Auto-generated method stub
 		return daoPiece.findAll();
 	}
+	
 	@Transactional
 	@Override
 	public Piece rechercherPieceId(int id) {
-		// TODO Auto-generated method stub
 		return daoPiece.findById(id).get();
 	}
+	
 	@Transactional
 	@Override
 	public void modifierPiece(Piece piece) {
 		daoPiece.save(piece);
-		
 	}
+	
 	@Transactional
 	@Override
 	public void supprimerPiece(Piece piece) {
 		daoPiece.deleteById(piece.getId());
-		
 	}
 
 }
