c3a8050767d423e4ee05de2ed7e7b407d4ac3ffc
CodeLinu <55236951+CodeLinu@users.noreply.github.com>
2019-11-07 12:57:37 +0100

    service

--- ProjetGestionGarages/src/main/java/com/garage/service/ServiceFactureEntretien.java
+++ ProjetGestionGarages/src/main/java/com/garage/service/ServiceFactureEntretien.java
@@ -2,9 +2,13 @@ package com.garage.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.garage.bean.FactureEntretien;
+import com.garage.dao.DaoFactureEntretien;
 import com.garage.iservice.IServiceFactureEntretien;
 
 /**
@@ -12,41 +16,46 @@ import com.garage.iservice.IServiceFactureEntretien;
  */
 @Service
 public class ServiceFactureEntretien implements IServiceFactureEntretien {
-
+	@Autowired DaoFactureEntretien factureentretienDao;
 	/**
 	 * Default constructor
 	 */
 	public ServiceFactureEntretien() {
 	}
 
+	@Transactional
 	@Override
 	public List<FactureEntretien> rechercheFactureEntretien() {
 		// TODO Auto-generated method stub
-		return null;
+		return factureentretienDao.findAll();
 	}
 
+	@Transactional
 	@Override
 	public FactureEntretien rechercherFactureEntretienId(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		return factureentretienDao.findById(id).get();
 	}
 
+	@Transactional
 	@Override
 	public void creerFactureEntretien(FactureEntretien facture) {
 		// TODO Auto-generated method stub
-		
+		factureentretienDao.save(facture);
 	}
 
+	@Transactional
 	@Override
 	public void modifierFactureEntretien(FactureEntretien facture) {
 		// TODO Auto-generated method stub
-		
+		factureentretienDao.save(facture);
 	}
 
+	@Transactional
 	@Override
 	public void supprimerFactureEntretien(FactureEntretien facture) {
 		// TODO Auto-generated method stub
-		
+		factureentretienDao.deleteById(facture.getId());
 	}
 
 }
