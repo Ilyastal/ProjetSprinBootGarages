5cef3a1a8cdb8ef3a75703c5026c249160ecd2d1
NesrineBacouch <55236882+NesrineBacouch@users.noreply.github.com>
2019-11-07 16:21:12 +0100

    form
    controllorForm

--- ProjetGestionGarages/src/main/java/com/garage/controlleur/form/CommandePieceForm.java
+++ ProjetGestionGarages/src/main/java/com/garage/controlleur/form/CommandePieceForm.java
@@ -1,5 +1,16 @@
 package com.garage.controlleur.form;
 
+import java.util.Date;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+
+import com.garage.bean.Piece;
+import com.garage.bean.Utilisateur;
+
 /**
  * 
  */
@@ -10,5 +21,88 @@ public class CommandePieceForm {
 	 */
 	public CommandePieceForm() {
 	}
+	private Integer id;
 
+	@NotEmpty
+	private Date dateCreation;
+	
+	@NotEmpty
+	private Date dateLivraison;
+	@NotEmpty
+	private Boolean reception;
+	@NotEmpty
+	private String description;
+	@NotEmpty
+	private Integer quantite;
+	
+	@NotEmpty
+	private Piece piece;
+	@NotEmpty
+	private Utilisateur utilisateur;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	public Date getDateLivraison() {
+		return dateLivraison;
+	}
+
+	public void setDateLivraison(Date dateLivraison) {
+		this.dateLivraison = dateLivraison;
+	}
+
+	public Boolean getReception() {
+		return reception;
+	}
+
+	public void setReception(Boolean reception) {
+		this.reception = reception;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(Integer quantite) {
+		this.quantite = quantite;
+	}
+	
+
+	public Piece getPiece() {
+		return piece;
+	}
+
+	public void setPiece(Piece piece) {
+		this.piece = piece;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
 }
--- ProjetGestionGarages/src/main/java/com/garage/controlleur/form/UtilisateurForm.java
+++ ProjetGestionGarages/src/main/java/com/garage/controlleur/form/UtilisateurForm.java
@@ -36,17 +36,17 @@ public class UtilisateurForm {
 	/**
 	 * 
 	 */
-	private String motpass;
+	private String password;
 
 	/**
 	 * 
 	 */
-	private Boolean desactiverUser;
+	private Boolean desactiveUser;
 
 	/**
 	 * 
 	 */
-	private Boolean desactiverAngular;
+	private Boolean desactiveAngular;
 
 	public Integer getId() {
 		return id;
@@ -81,27 +81,27 @@ public class UtilisateurForm {
 	}
 	
 	public String getMotpass() {
-		return motpass;
+		return password;
 	}
 
 	public void setMotpass(String motpass) {
-		this.motpass = motpass;
+		this.password = motpass;
 	}
 
 	public Boolean getDesactiverUser() {
-		return desactiverUser;
+		return desactiveUser;
 	}
 
 	public void setDesactiverUser(Boolean desactiverUser) {
-		this.desactiverUser = desactiverUser;
+		this.desactiveUser = desactiverUser;
 	}
 
 	public Boolean getDesactiverAngular() {
-		return desactiverAngular;
+		return desactiveAngular;
 	}
 
 	public void setDesactiverAngular(Boolean desactiverAngular) {
-		this.desactiverAngular = desactiverAngular;
+		this.desactiveAngular = desactiverAngular;
 	}
 
 }
