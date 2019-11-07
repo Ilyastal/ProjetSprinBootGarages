7f5a4c991bea2a2f64d434f71011eb93a1a6d8a4
NesrineBacouch <55236882+NesrineBacouch@users.noreply.github.com>
2019-11-07 12:19:06 +0100

    servicePiece
    pieces

--- ProjetGestionGarages/src/main/java/com/garage/service/ServiceCommandeVoiture.java
+++ ProjetGestionGarages/src/main/java/com/garage/service/ServiceCommandeVoiture.java
@@ -2,9 +2,12 @@ package com.garage.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import com.garage.bean.CommandeVoiture;
+import com.garage.dao.DaoCommandeVoiture;
 import com.garage.iservice.IServiceCommandeVoiture;
 
 /**
@@ -12,49 +15,48 @@ import com.garage.iservice.IServiceCommandeVoiture;
  */
 @Service
 public class ServiceCommandeVoiture implements IServiceCommandeVoiture {
-
+	DaoCommandeVoiture daoCommandeVoiture;
 	/**
 	 * Default constructor
 	 */
 	public ServiceCommandeVoiture() {
 	}
-
+	@Transactional
 	@Override
 	public List<CommandeVoiture> rechercheCommandeVoiture() {
 		// TODO Auto-generated method stub
-		return null;
+		return daoCommandeVoiture.findAll();
 	}
-
+	@Transactional
 	@Override
 	public CommandeVoiture rechercherCommandeVoitureId(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		return daoCommandeVoiture.findById(id).get();
 	}
-
+	@Transactional
 	@Override
 	public void creerCommandeVoiture(CommandeVoiture commandeVoiture) {
-		// TODO Auto-generated method stub
-		
+		daoCommandeVoiture.save(commandeVoiture);
 	}
-
+	@Transactional
 	@Override
 	public void modifierCommandeVoiture(CommandeVoiture commandeVoiture) {
-		// TODO Auto-generated method stub
+		daoCommandeVoiture.save(commandeVoiture);
 		
 	}
-
+	@Transactional
 	@Override
 	public void supprimerCommandeVoiture(CommandeVoiture commandeVoiture) {
-		// TODO Auto-generated method stub
+		daoCommandeVoiture.deleteById(commandeVoiture.getId());
 		
 	}
-
+	@Transactional
 	@Override
 	public List<CommandeVoiture> listerCommandeValidee() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Transactional
 	@Override
 	public List<CommandeVoiture> listerCommandeLivree() {
 		// TODO Auto-generated method stub
