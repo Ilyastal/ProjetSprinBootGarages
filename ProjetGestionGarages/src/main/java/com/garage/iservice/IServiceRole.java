f95be964ab996743a7f5f36095a045107c383c89
Ilyastal <47531017+Ilyastal@users.noreply.github.com>
2019-11-08 14:23:22 +0100

    Service role

--- ProjetGestionGarages/src/main/java/com/garage/iservice/IServiceRole.java
+++ ProjetGestionGarages/src/main/java/com/garage/iservice/IServiceRole.java
@@ -1,8 +1,19 @@
 package com.garage.iservice;
 
+import java.util.List;
+
+import com.garage.bean.Role;
+
 /**
  * 
  */
 public interface IServiceRole {
+	
+	public List<Role> rechercheRole();
+	public Role rechercherRoleId(final int id);
+	public void creerRole(final Role role);
+	public void modifierRole(final Role role);
+	public void supprimerRole(final Role role);
+	
 
 }
--- ProjetGestionGarages/src/main/java/com/garage/service/ServiceRole.java
+++ ProjetGestionGarages/src/main/java/com/garage/service/ServiceRole.java
@@ -1,16 +1,58 @@
 package com.garage.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.garage.bean.Role;
+import com.garage.dao.DaoRole;
 import com.garage.iservice.IServiceRole;
 
 /**
  * 
  */
+@Service
 public class ServiceRole implements IServiceRole {
 
-	/**
-	 * Default constructor
-	 */
-	public ServiceRole() {
+	@Autowired
+	private DaoRole roleDao;
+	
+	@Transactional
+	@Override
+	public List<Role> rechercheRole() {
+		
+		return roleDao.findAll();
+	}
+
+	@Transactional
+	@Override
+	public Role rechercherRoleId(int id) {
+		
+		return roleDao.findById(id).get();
+	}
+
+	@Transactional
+	@Override
+	public void creerRole(Role role) {
+		roleDao.save(role);
+		
+	}
+
+	@Transactional
+	@Override
+	public void modifierRole(Role role) {
+		roleDao.save(role);
+		
+	}
+
+	@Transactional
+	@Override
+	public void supprimerRole(Role role) {
+		roleDao.deleteById(role.getId());
+		
 	}
 
 }
